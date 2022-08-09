2ac867f0a06b903f1e2941247896ff0643935805
andrii.dudar <fanat.web@gmail.com>
2022-08-09 14:47:31 +0200

    - add model graph

--- heavy_loaded.py
+++ heavy_loaded.py
@@ -10,15 +10,18 @@ experiment = Experiment(
     workspace="andriidudar",
 )
 
+experiment.set_model_graph('0-> 2-> 3-> 4', overwrite=False)
+
 for i in range(50):
-    for j in range(1000):
-        sleep(0.1)
-        experiment.log_metric("metric_" + str(j), i + randrange(10), step=i)
+    for j in range(200):
+        sleep(0.005)
+        experiment.log_metric("metric_" + str(j), min(max(i + randrange(20), 0), 100), step=i)
         
     
 for i in range(1000):
-    sleep(0.1)
-    experiment.log_parameter('param' + str(i), 'random_value_' + str(randrange(10)))
+    sleep(0.005)
+    experiment.log_parameter('param_' + str(i), 'random_value_' + str(randrange(10)))
+
 
 
 experiment.end()
