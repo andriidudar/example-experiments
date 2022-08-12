b89dc7517b67b05803296fc6e21d2448cea5ef59
andrii.dudar <fanat.web@gmail.com>
2022-08-12 14:01:07 +0200

    - add model to experiment

--- heavy_loaded_all.py
+++ heavy_loaded_all.py
@@ -1,5 +1,5 @@
 from time import sleep
-from random import randrange, random
+from random import randrange, random, getrandbits
 
 import numpy as numpy
 from comet_ml import Experiment
@@ -24,7 +24,7 @@ desired_output = [
     [randrange(1), randrange(1), randrange(1)],
     [randrange(1), randrange(1), randrange(1)],
     [randrange(1), randrange(1), randrange(1)],
- ]
+]
 
 actual_output = [
     [randrange(1, step=0.1), randrange(1, step=0.1), randrange(1, step=0.1)],
@@ -33,7 +33,7 @@ actual_output = [
     [randrange(1, step=0.1), randrange(1, step=0.1), randrange(1, step=0.1)],
     [randrange(1, step=0.1), randrange(1, step=0.1), randrange(1, step=0.1)],
     [randrange(1, step=0.1), randrange(1, step=0.1), randrange(1, step=0.1)],
- ]
+]
 
 experiment.log_confusion_matrix(desired_output, actual_output)
 
@@ -42,18 +42,6 @@ experiment.set_model_graph('0 -> ' + str(randrange(3)) + ' -> ' + str(randrange(
 
 # -------------------- HTML
 experiment.log_html('<a href="www.comet.ml"> I love Comet.ml ' + str(randrange(5)) + 'times </a>')
-
-# -------------------- 3D Histogram
-for i in range(steps):
-    experiment.log_histogram_3d(numpy.array([randrange(5), randrange(5), randrange(5)]), step=i)
-
-# -------------------- graphics
-for i in range(steps):
-    experiment.log_image('./images/image_' + str(randrange(7) + 1) + '.png', step=i)
-    experiment.log_image('./images/image_' + str(randrange(7) + 1) + '.png', step=i)
-    experiment.log_image('./images/image_' + str(randrange(7) + 1) + '.png', step=i)
-    experiment.log_image('./images/image_' + str(randrange(7) + 1) + '.png', step=i)
-    experiment.log_image('./images/image_' + str(randrange(7) + 1) + '.png', step=i)
 
 # -------------------- audio
 experiment.log_audio('./audios/zero/0ab3b47d_nohash_0.wav', file_name='zero')
@@ -67,22 +55,33 @@ experiment.log_audio('./audios/seven/0a0b46ae_nohash_0.wav', file_name='seven')
 experiment.log_audio('./audios/eight/0a9f9af7_nohash_0.wav', file_name='eight')
 experiment.log_audio('./audios/nine/0a7c2a8d_nohash_0.wav', file_name='nine')
 
-# -------------------- text
+# -------------------- model
+if bool(getrandbits(1)):
+    experiment.log_model('Heavy Loaded', 'heavy_loaded.py')
+else:
+    experiment.log_model('Heavy Images', 'heavy_loaded_images.py')
+
 for i in range(steps):
+    sleep(2)
+    # -------------------- 3D Histogram
+    experiment.log_histogram_3d(numpy.array([randrange(5), randrange(5), randrange(5)]), step=i)
+
+    # -------------------- graphics
+    experiment.log_image('./images/image_' + str(randrange(7) + 1) + '.png', step=i)
+    # experiment.log_image('./images/image_' + str(randrange(7) + 1) + '.png', step=i)
+    # experiment.log_image('./images/image_' + str(randrange(7) + 1) + '.png', step=i)
+    # experiment.log_image('./images/image_' + str(randrange(7) + 1) + '.png', step=i)
+    # experiment.log_image('./images/image_' + str(randrange(7) + 1) + '.png', step=i)
+
+    # -------------------- text
     experiment.log_text('random text' + str(random()), step=i)
 
-# -------------------- metrix
-for i in range(steps):
+    # -------------------- params
+    experiment.log_parameter('param_' + str(i), 'random_value_' + str(randrange(10)))
+
     for j in range(metrics):
         sleep(0.005)
         experiment.log_metric("metric_" + str(j), min(max(i + randrange(20), 0), 100), step=i)
-
-# -------------------- params
-
-for i in range(params):
-    sleep(0.005)
-    experiment.log_parameter('param_' + str(i), 'random_value_' + str(randrange(10)))
-
 
 
 experiment.end()
